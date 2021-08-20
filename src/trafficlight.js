177f6cc3e31d5f297656773e6984e7186898821f
sebastianignacioqa <sebastianignacioqa@gmail.com>
2021-08-20 15:15:00 -0400

    First stage

---
+++ src/trafficlight.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./trafficlight.css";
+
+const TrafficLight = ()=> {
+    return (<div className ="trafficlight">
+            <div className ="redlight"></div>
+            <div className = "orangelight"></div>
+            <div className = "greenlight"></div>
+            </div>)
+}
+
+
+
+
+export default TrafficLight
